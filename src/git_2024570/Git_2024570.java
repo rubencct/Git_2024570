54a4182277a77490064f1aaac31e2c272f1891fd
rubencct <2024570@student.cct.ie>
2024-12-02 16:59:28 +0000

    Added a simple print showing student name and student ID.

---
+++ src/git_2024570/Git_2024570.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2024570;
+
+/**
+ *
+ * @author ruben_1d
+ */
+public class Git_2024570 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Name: Mario Rubén Arturo Vera Rodriguez"); //Simple script to display my Name and Surname
+        System.out.println("Student ID: 2024570"); //Simple printig to show my Student Number.
+        
+    }
+    
+}
